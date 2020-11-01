ed0bf8713bab522e0692d6563bbbad4a5b357eae
saoussen <sawsan_sli3i@live.fr>
2020-11-01 14:39:38 +0000

    modified test tail with use of require chai

--- test/testTail.js
+++ test/testTail.js
@@ -1,5 +1,5 @@
 const tail = require('../tail');
-const assert = require('assert')
+const assert = require('chai').assert;
 
 it('should return true', () => {
   assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
