bfe7cc0dfa0656aa40d98055720623c5eb09690d
saoussen <sawsan_sli3i@live.fr>
2020-11-01 14:56:24 +0000

    added and edited the index file

---
+++ index.js
@@ -0,0 +1,21 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const count = require('./countOnly');
+const countL = require('./countLetters');
+const letterP = require('./letterPositions');
+const map = require('./map');
+const pigLatin = require('./pigLatin');
+const reverseString = require('./reverseString');
+const takeUntil = require('./takeUntil');
+const without = require ('./without');
+const moduleChecks = require('./moduleCheck');
+const eqO= require('./eqObjects');
+const eqA= require('./eqArrays');
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle
+};
